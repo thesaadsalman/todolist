265043431710b6920671a3d181067ccb283b12ca
Saad Salman <saaduhbucs@gmail.com>
2020-01-29 14:51:27 -0800

    Removed bug with Math.random

--- src/TodoEntryForm.js
+++ src/TodoEntryForm.js
@@ -1,11 +1,12 @@
 import React, { useState } from 'react';
+import uuidv4 from 'uuid/v4';
 
 export default function TodoEntryForm( { adder }) {
   const [content, setContent] = useState('');
   function submit(e) {
     e.preventDefault();
     if(content.trim()){
-    adder({ id: Math.random(), content });
+    adder({ uiid: uuidv4(), content });
     }
     setContent('');
   }
