1542cb81e1adca5a3528b5e284fdc50508dc73a2
Saad Salman <saaduhbucs@gmail.com>
2020-01-29 14:44:50 -0800

    Add functionality: Add Todo

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import TodoList from './TodoList'
+import TodoEntryForm from './TodoEntryForm';
 
 export default function App() {
   const [todos, setTodos] = useState([
@@ -9,10 +10,14 @@ export default function App() {
   function deleteTodo(id) {
     setTodos(todos => todos.filter(todo => todo.id !== id));
   }
+  function addTodo(todo) {
+  setTodos(todos => [...todos, todo]);
+  }
   return (
     <div className="App">
       <h1>Things To Do</h1>
       <TodoList todos={todos} deleter={deleteTodo} />
+      <TodoEntryForm adder={addTodo} />
     </div>
   );
 }
